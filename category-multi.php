ea26d99ee242b14aab9fc4d8536f13c7f53ce71b
jeremyclarke <jer@simianuprising.com>
2013-05-30 15:56:29 -0400

    update category and multicategory to have meta stuff in sidebar

--- category-multi.php
+++ category-multi.php
@@ -4,10 +4,10 @@
 <div class="row">
 	<div class="small-12 columns">
 		<h2 class="inline-block">
-			<a href=''>CATEGORY NAME</a> or <a href=''><?php echo phpsum_words(1, 3)?></a> or <a href=''><?php echo phpsum_words(1, 3)?></a>
+			<a href=''>Americas</a>
 		</h2>
-		<p>Posts in any of these categories. <i>[replace "any" with "all" on this line and "or" with "and" above for union instead of intersection]</i></p>
-		<div class='panel hide-for-small'>
+		<span style='padding-left:1em;font-weight:bold;'>Posts in <a href='category.php'>Caribbean</a> or <a href='category.php'>Latin America</a> or <a href='category.php'>North America</a></span>
+<!--		<div class='panel hide-for-small'>
 			<ul class='inline-list'>
 				<?php
 				$i=0;				
@@ -19,37 +19,10 @@
 				?>
 				<li><i>[Child Categories of the various categories. All together? Separate?]</i></li>
 			</ul>
-		</div>
+		</div>-->
 		
 	</div>
-	<div class="small-12 columns hide-for-mobile">
-		<span class="button small secondary"><a>RSS for these categories</a></span>
-		<span class="button small secondary dropdown inline-block" style='margin-left:1em' data-dropdown="drop-faceted">
-			Refine results
-		</span>
 
-		<div id="drop-faceted" class="f-dropdown medium content" data-dropdown-content>
-		  <p>Options to mix in a second category or remove a category</p>
-		</div>				
-	
-<!--		<form class='custom inline-block'>
-			 <select id="customDropdown1" class="medium">
-			   <option>Also in PARENT</option>
-			   <option>Sibling Category</option>
-			   <option>Sibling Category</option>
-			   <option>Sibling Category</option>
-			 </select>
-		</form>		-->
-
-		<form class='custom inline-block'>
-			 <select id="customDropdown1" class="medium">
-			   <option>Monthly Archives for these categories</option>
-			   <option>Month</option>
-			   <option>Month</option>
-			   <option>Month</option>
-			 </select>
-		</form>
-	</div>
 	<div class="show-for-mobile small-12 columns">
 		<span class="button small secondary dropdown " data-dropdown="drop-cat-tools" style="width:100%">
 			Category Tools
@@ -82,13 +55,56 @@
 </div>
 	<div class="row hero">
 		<?php include('_featured-posts.php');?>
-		<div class="large-4 columns">
+				<div class="large-4 columns hide-for-mobile">
 
-			<div class="panel">
-				<p>[We can't show category description here because it's likely to be way too long if we show one for each of the multi-categories.]</p>
-				<p>[So what to show??? Homepage promo box? Post headlines?]</p>
+			<span class="button small secondary"><a>RSS for Category</a></span>
+			<span class="button small secondary dropdown inline-block" style='margin-left:1em' data-dropdown="drop-faceted">
+				Refine results
+			</span>
+
+			<div id="drop-faceted" class="f-dropdown medium content" data-dropdown-content>
+			  <p>Options to mix in a second category or remove a category</p>
 			</div>
-
+			
+			<form class='custom inline-block'>
+			 <select id="customDropdown1" class="medium">
+			   <option>Countries in Caribbean</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			 </select>
+			 <select id="customDropdown1" class="medium">
+			   <option>Countries in Latin America</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			 </select>
+			 <select id="customDropdown1" class="medium">
+			   <option>Countries in North America</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			 </select>
+				
+			 <select id="customDropdown1" class="medium">
+			   <option>Monthly Archives for Category</option>
+			   <option>Month</option>
+			   <option>Month</option>
+			   <option>Month</option>
+			 </select>
+			</form>
 
 		</div>
 	</div>
--- category.php
+++ category.php
@@ -4,9 +4,9 @@
 <div class="row">
 	<div class="small-12 columns">
 		<h2 class="inline-block">
-			CATEGORY NAME <small class='hide-for-mobile'> &laquo; <A>PARENT</a> &laquo; <a>GRANDPARENT</a></small>
+			Dominican Republic<small class='hide-for-mobile'> &laquo; <a href='category.php'>Latin America</a> &laquo; <a href='category.php'>World</a></small>
 		</h2>
-		<div class='panel hide-for-small'>
+<!--		<div class='panel hide-for-small'>
 			<ul class='inline-list'>
 				<?php
 				$i=0;
@@ -21,37 +21,10 @@
 				endwhile;
 				?>
 			</ul>
-		</div>
+		</div>-->
 		
 	</div>
-	<div class="small-12 columns hide-for-mobile">
-		<span class="button small secondary"><a>RSS for Category</a></span>
-		<span class="button small secondary dropdown inline-block" style='margin-left:1em' data-dropdown="drop-faceted">
-			Refine results
-		</span>
 
-		<div id="drop-faceted" class="f-dropdown medium content" data-dropdown-content>
-		  <p>Options to mix in a second category or remove a category</p>
-		</div>				
-	
-<!--		<form class='custom inline-block'>
-			 <select id="customDropdown1" class="medium">
-			   <option>Also in PARENT</option>
-			   <option>Sibling Category</option>
-			   <option>Sibling Category</option>
-			   <option>Sibling Category</option>
-			 </select>
-		</form>		-->
-
-		<form class='custom inline-block'>
-			 <select id="customDropdown1" class="medium">
-			   <option>Monthly Archives for Category</option>
-			   <option>Month</option>
-			   <option>Month</option>
-			   <option>Month</option>
-			 </select>
-		</form>
-	</div>
 	<div class="show-for-mobile small-12 columns">
 		<span class="button small secondary dropdown " data-dropdown="drop-cat-tools" style="width:100%">
 			Category Tools
@@ -76,7 +49,7 @@
 			   <option>Month</option>
 			 </select>
 			</form>
-					<span class="button small secondary"><a>Refine Results</a></span>
+			<span class="button small secondary"><a>Refine Results</a></span>
 
 		</div>				
 
@@ -84,12 +57,40 @@
 </div>
 	<div class="row hero">
 		<?php include('_featured-posts.php');?>
-		<div class="large-4 columns">
+		<div class="large-4 columns hide-for-mobile">
 
+			<span class="button small secondary"><a>RSS for Latin America</a></span>
+			<span class="button small secondary dropdown inline-block" style='margin-left:1em' data-dropdown="drop-faceted">
+				Refine results
+			</span>
+
+			<div id="drop-faceted" class="f-dropdown medium content" data-dropdown-content>
+			  <p>Options to mix in a second category or remove a category</p>
+			</div>
+			
+			<form class='custom inline-block'>
+			 <select id="customDropdown1" class="medium">
+			   <option>Countries in CATEGORY NAME</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			   <option>CHILD COUNTRY</option>
+			 </select>
+				 <select id="customDropdown1" class="medium">
+				   <option>Monthly Archives for Category</option>
+				   <option>Month</option>
+				   <option>Month</option>
+				   <option>Month</option>
+				 </select>
+			</form>
 			<div class="panel">
 				<p>[Content from description field of category. Relevant editors, special funders etc.]</p>
-				<p>[IF NO CUSTOM CONTENT: ??? Homepage promo box? Post headlines?]</p>
+				<p></p>
 			</div>
+
 
 
 		</div>
@@ -99,11 +100,11 @@
 
 		<div class='row'>
 			<div class="large-8 columns">
-				<h3>LATEST POSTS ABOUT CATEGORY NAME</h3>
+				<h3>Latest Dominican Republic Stories	</h3>
 				<?php include('_post-summaries.php') ?>
 			</div>
 			<div class="large-4 columns">
-				<h3>QUICK READS</h3>
+				<h3>Quick Reads</h3>
 				<?php include('_quickreads.php');?>
 			</div>
 		</div>			
