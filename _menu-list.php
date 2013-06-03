e03fde208d394d3da7037bb2fc63d1f141969418
jeremyclarke <jer@simianuprising.com>
2013-06-03 15:33:37 -0400

    remove category-special template and it's link in menu_list

--- _menu-list.php
+++ _menu-list.php
@@ -2,7 +2,6 @@
 				<li><a href='index.php'>Home Wireframe</a></li>
 				<li><a href='category.php'>Category Wireframe</a></li>
 				<li><a href='category-multi.php'>Multi-Category Wireframe</a></li>
-				<li><a href='category-special.php'>Special Category</a></li>
 				<li><a href='author.php'>Author Wireframe</a></li>
 				<li><a href='single.php'>Post Wireframe</a></li>
 				<li><a href='page.php'>Page Wireframe</a></li>
